a7903bd0cdbe1af06998486f7ab1319ce392370f
Francky <foziefranck@gmail.com>
2025-11-10 11:09:04 +0100

    modifying the database to display only first and last names

--- TP-1/backend/database.sql
+++ TP-1/backend/database.sql
@@ -1,3 +1,5 @@
 CREATE TABLE book (title character varying(100) NOT NULL);
 INSERT INTO book (title)
-VALUES ('Devops&CD'),('DevOps'), ('Efrei'), ('Cloud'), ('CI/CD');
+#VALUES ('Devops&CD'),('DevOps'), ('Efrei'), ('Cloud'), ('CI/CD');
+# Modifying database to display only first and last name
+VALUES  ('Franck Abdiel'), ('KENNE FOZIE');
