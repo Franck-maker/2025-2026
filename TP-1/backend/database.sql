402f15abe36178cf31ad5e4cd0826cbbec03e9b7
Francky <foziefranck@gmail.com>
2025-11-19 11:37:13 +0100

    adding kubernetes deployment files and fixing the docker compose and the database

--- TP-1/backend/database.sql
+++ TP-1/backend/database.sql
@@ -1,5 +1,3 @@
 CREATE TABLE book (title character varying(100) NOT NULL);
 INSERT INTO book (title)
-#VALUES ('Devops&CD'),('DevOps'), ('Efrei'), ('Cloud'), ('CI/CD');
-# Modifying database to display only first and last name
 VALUES  ('Franck Abdiel'), ('KENNE FOZIE');
